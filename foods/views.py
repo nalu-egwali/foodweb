486ec4d212f13a5fc18f8cfb6cec49a6670332aa
natty-king <egwalichukwunalu@gmail.com>
2024-06-16 20:22:45 +0100

    Add Foodcard for displaying available foods

--- foods/views.py
+++ foods/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from . import urls
 
 # Create your views here.
 def index (request):
     return render(request, 'home.html', {})
+
+def login_user (request):
+    if request.method == 'POST':
+        return HttpResponse("You are logged in")
+    return render(request, 'loginform.html', {})
+
+def logout_user (request):
+    logout(request)
+    return HttpResponse (" You are loggod out")

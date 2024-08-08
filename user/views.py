29f2739c16d02f1270e1267111ad9fc51905ec0a
Chukwunalu Egwali <egwalichukwunalu@gmail.com>
2024-08-08 21:36:33 +0100

    Add POST request handler to the sign up view

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from .forms import SignUpForm
@@ -13,8 +13,16 @@ def login_user (request):
 
 def logout_user (request):
     logout(request)
-    return HttpResponse (" You are loggod out")
+    return HttpResponse ("You are loggod out")
 
 def signup (request):
-    form = SignUpForm()
-    return render (request, 'signup.html', {'form': form})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/user/login/')
+        else:
+            return HttpResponse('There was an error in your input')
+    else:    
+        form = SignUpForm()
+        return render (request, 'signup.html', {'form': form})

ec66a8a249541eb196b3482314438b816bcfe1f2
Chukwunalu Egwali <egwalichukwunalu@gmail.com>
2024-09-03 23:20:02 +0100

    ADD(ui): Add delete button to food item details
    This commit adds delete funtionality to enable vendors
    to delete any food that they posted. It also updates
    the food details page to include delete button.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,5 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('food/<int:pk>/', views.delete_food, name='delete_food'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from foods.models import Food
 
 # Create your views here.
@@ -9,3 +9,10 @@ from foods.models import Food
 def index(request):
     foods = Food.objects.filter(created_by = request.user)
     return render (request, 'dashboard/index.html', {'foods': foods})
+
+@login_required
+def delete_food(request, pk):
+    food_item = get_object_or_404(Food, pk, created_by = request.user)
+    food_item.delete()
+
+    return redirect('/dashboard/index/')
